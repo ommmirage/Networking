9b9940d9dc7bb5215ee465e6a967e0e8a8d9aa9a
Sergey Miroshnichenko <ommmirage@gmail.com>
2023-06-02 16:25:28 +0400

    tasks.json -std=c++17

--- main.cpp
+++ main.cpp
@@ -1,8 +1,65 @@
 #include <iostream>
 
 #include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+std::vector<char> vBuffer(20 * 1024);
+
+void GrabSomeData(asio::ip::tcp::socket& socket)
+{
+    // It will prime the context with some work to do when data is available on this
+    // socket to read
+    socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+        [&](std::error_code ec, std::size_t length)
+        {
+            if (!ec)
+            {
+                std::cout << "\n\nRead " << length << " bytes\n\n";
+
+                for (int i = 0; i < length; i++)
+                    std::cout << vBuffer[i];
+
+                GrabSomeData(socket);
+            }
+        }
+    );
+}
 
 int main()
 {
-    std::cout << "QQ" << std::endl;
+    asio::error_code ec;
+
+    // Create a context - essentially the platform specific interface
+    asio::io_context context;
+
+    // Get the address of somewhere we wish to connect to
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+    // The context will deliver the implementation
+    asio::ip::tcp::socket socket(context);
+
+    // Tell socket to try to connect
+    socket.connect(endpoint, ec);
+
+    if (!ec)
+    {
+        std::cout << "Connected!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
+    }
+
+    if (socket.is_open())
+    {
+        std::string sRequest =
+            "GET /index.html HTTP/1.1\r\n"
+            "Host: example.com\r\n"
+            "Connection: close\r\n\r\n";
+
+        socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+        GrabSomeData(socket);
+    }
 }
