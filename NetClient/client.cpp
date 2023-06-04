af7d2e60187619a9028971eb39c39a95a12d6ac4
Sergey Miroshnichenko <ommmirage@gmail.com>
2023-06-04 17:09:12 +0400

    1

---
+++ NetClient/client.cpp
@@ -0,0 +1,7 @@
+#include "../NetCommon/net_message.h"
+
+int main()
+{
+    message msg;
+    msg.header.type = MsgType::MovePlayer;
+}
---
+++ NetCommon/net_common.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <optional>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
